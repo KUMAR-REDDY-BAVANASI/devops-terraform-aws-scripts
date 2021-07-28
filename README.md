cd7c8f028f83a0e572eedf6953b83b7bf46e5e9b
nages <yarrakondaka>
2021-07-28 15:28:20 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# devops-petclinic
+# devops-petclinic
---
+++ main.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = "AKIA5UQMJGY5FBQKEDHV"
+  secret_key = "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"
+  region     = "ap-south-1"
+}
